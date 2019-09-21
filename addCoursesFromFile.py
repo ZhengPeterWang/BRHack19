3f14f8aabcc821a4a7bf739b9973f66311cce04a
Zheng Wang <zw494@cornell.edu>
2019-09-21 16:21:07 -0400

    3

--- addCoursesFromFile.py
+++ addCoursesFromFile.py
@@ -1,16 +1,19 @@
+
 import os
 import sqlite3   # Enable control of an SQLite database
 import sys
 import json
+con = sqlite3.connect("database.db")
+cur = con.cursor()
 
 with open('CS.json', 'r') as f:
     fileDict = json.load(f)
-classes = fileDict["data"]["classes"]
-data = []
-for c in classes:
-    for section in classes["enrollGroups"]["classSections"]:
-        for meeting in section["meetings"]:
-            data.append((c["ssrComponent"], section["classId"], c["titleLong"], )
+    classes = fileDict["data"]["classes"]
+    data = []
+    for cl in classes:
+        for section in cl["enrollGroups"]["classSections"]:
+            for meeting in section["meetings"]:
+                data.append((cl["ssrComponent"], section["classId"], cl["titleLong"], meeting["pattern"], meeting["timeStart"], meeting["timeEnd"], cl["crseId"] )
+cur.executemany("INSERT INTO database VALUES (?, ?, ?, ?, ?, ?, ?)", data)
 
-cur.executemany("INSERT INTO samples VALUES (?, ?, ?, ?, ?, ?, ?)", data)
 
